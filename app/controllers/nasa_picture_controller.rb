2ed4f09a05a5c5786333469b112b08109bc9cdbf
kotun08 <mkotun@gmail.com>
2020-07-30 08:22:32 +0100

    add controller and view

---
+++ app/controllers/nasa_picture_controller.rb
@@ -0,0 +1,7 @@
+class NasaPictureController < ApplicationController
+  def index
+    @nasa_info = Nasa.get_api_info
+    @media_type_is_video = @nasa_info['media_type'].eql?('video')
+
+  end
+end
--- app/models/nasa.rb
+++ app/models/nasa.rb
@@ -1,2 +1,8 @@
 class Nasa < ApplicationRecord
+    def self.get_api_info
+        nasa = "https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}"
+        request_to_nasa = Net::HTTP.get(URI(nasa))
+        JSON.parse request_to_nasa
+      end
+
 end
